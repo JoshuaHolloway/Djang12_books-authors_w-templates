1869cbb792f0b7c532752aad74914fb28ec9922c
joshuaholloway <jhollow6@asu.edu>
2019-06-17 15:26:34 -0500

    passing in data from python into HTML

--- books_authors/apps/books_authors_app/views.py
+++ books_authors/apps/books_authors_app/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "books_authors_app/index.html")
+    context = {
+        "name": "Noelle",
+        "favorite_color": "turquoise",
+        "pets": ["Bruce", "Fitz", "Georgie"]
+    }
+    return render(request, "books_authors_app/index.html", context)
+    # return render(request, "books_authors_app/index.html")
     # return HttpResponse("this is the equivalent of @app.route('/')!")
