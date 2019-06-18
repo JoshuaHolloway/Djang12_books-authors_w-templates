6a0e918b93aa676c5bd71f4ca05e49d0bc80dfad
joshuaholloway <jhollow6@asu.edu>
2019-06-18 15:14:46 -0500

    listing all books author has written/co-written on their show page

--- books_authors/apps/books_authors_app/urls.py
+++ books_authors/apps/books_authors_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
   url(r'^$', views.index),
   url(r'^authors$', views.authors),
   url(r'^books/add$', views.add),
-  url(r'^books/(?P<my_val>\d+)$', views.show),   # e.g., localhost:8000/bears/23
-  url(r'^books/assign_author/(?P<book_id>\d+)/(?P<author_id>\d+)$', views.assign_author)   # e.g., localhost:8000/bears/23
+  url(r'^books/(?P<my_val>\d+)$', views.show),   # e.g., localhost:8000/books/23
+  url(r'^authors/(?P<my_val>\d+)$', views.show_authors),   # e.g., localhost:8000/authors/23
+  url(r'^books/assign_author/(?P<book_id>\d+)/(?P<author_id>\d+)$', views.assign_author),
+  url(r'^authors/assign_book/(?P<author_id>\d+)/(?P<book_id>\d+)', views.assign_book)
 ]
--- books_authors/apps/books_authors_app/views.py
+++ books_authors/apps/books_authors_app/views.py
@@ -50,3 +50,40 @@ def assign_author(request, book_id, author_id):
 # ======================================================================================================================
 def authors(request):
   return render(request, "books_authors_app/authors.html", {"authors": Author.objects.all()})
+# ======================================================================================================================
+def show_authors(request, my_val):
+
+  author = Author.objects.get(id=my_val)
+  print(author.name)
+
+  # Grab list of all books this author has written/co-written
+  books_authored = author.books.all()
+
+  context = {"author": author, "books_authored": books_authored, "books": Book.objects.all()}
+  print(context['author'])
+
+
+
+
+  if request.method == "GET":
+    print("a GET request is being made to this route")
+  if request.method == "POST":
+    print("a POST request is being made to this route")
+
+  # return HttpResponse("books/" + str(my_val))
+  return render(request, "books_authors_app/show_author.html", context)
+# ======================================================================================================================
+def assign_book(request, author_id, book_id):
+
+  print('INSIDE ASSIGN_BOOK()')
+
+  author = Author.objects.get(id=author_id)
+  book = Book.objects.get(id=book_id)
+
+  book.authors.add(author)
+
+  # DEBUG
+  print("Book-Title: " + str(book.title) + " has authors: " + str(book.authors.all()))
+
+  return redirect("/")
+# ======================================================================================================================
